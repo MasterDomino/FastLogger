06b6b8139973a0986fb65fad580cd2c0a627dd63
Dominik Grudziński <mrdominogrm@gmail.com>
2018-09-07 10:39:51 +0200

    add test cases

--- FastLogging/Program.cs
+++ FastLogging/Program.cs
@@ -8,6 +8,12 @@ namespace FastLogging.TestConsole
         {
             Logger.InitializeLogger(new Log(typeof(Program), LogLevel.ALL));
             Logger.Info("Logger initialized!");
+            Logger.Debug("Test debug message.");
+            Logger.Info("Test info message.");
+            Logger.Warn("Test warn message.");
+            Logger.Error("Test error message.");
+            Logger.Fatal("Test fatal message.");
+            Logger.Info("Test finished.");
             Console.ReadKey();
         }
     }
